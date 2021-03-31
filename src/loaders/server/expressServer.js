e93b9f5c994d72edfc66994f5ad4f16a53545631
Fer Valdes <fervaldes11@gmail.com>
2021-03-31 20:25:27 -0300

    commit only for test gitflow

--- src/loaders/server/expressServer.js
+++ src/loaders/server/expressServer.js
@@ -34,6 +34,10 @@ class ExpressServer {
             res.status(200).end();
         });
 
+        this.app.get("/gitflow", (req, res) => {
+            res.status(200).json({prueba: 'gitflow'});
+        });
+
         this.app.use(this.basePathUser, require('../../routes/users'));
     }
 
